07820f6667964f9e6cf0bccdee77f2d07a1f43b4
Matthew Laver <matthewlaver@gmail.com>
2012-03-03 20:45:30 +0000

    added site and tidied up folder

---
+++ stuff.js
@@ -0,0 +1,22 @@
+(function ($) {
+	$(document).ready(function () {
+	
+		$('.flexslider').flexslider({
+			animation: "slide",
+			controlsContainer: ".flex-container"
+		});
+		
+		function deg () {
+			var angles = [-3,-2,2,3];
+			return angles[Math.floor(Math.random()*angles.length)];
+		}
+		$('#sponsorShots').find('li').each(function (i) {
+			$(this).css({
+				"-webkit-transform": 'rotate(' + deg() + 'deg)',
+				"-moz-transform": 'rotate(' + deg() + 'deg)'
+			});
+		});
+		
+		
+	});
+}(jQuery))
