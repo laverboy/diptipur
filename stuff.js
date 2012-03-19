a088f66968decbd620f67f309946d826f46e98d2
Matthew Laver <matthewlaver@gmail.com>
2012-03-19 21:30:55 +0000

    added nice scroll

--- stuff.js
+++ stuff.js
@@ -18,5 +18,17 @@
 		});
 		
 		
+		/* ======= Smooth Scrolling ======= */
+		$("a.topLink").click(function() {
+			$("html, body").animate({
+				scrollTop: $($(this).attr("href")).offset().top + "px"
+			}, {
+				duration: 500,
+				easing: "swing"
+			});
+			return false;
+		});
+		
+		
 	});
 }(jQuery))
